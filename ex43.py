f0bb6f903354f5e614ee790840f410ea60b10401
Quoc Ly <student.140216@worcesterschools.net>
2019-06-04 16:27:07 +0000

    35-45

--- ex43.py
+++ ex43.py
@@ -5,11 +5,12 @@ from textwrap import dedent
 class Scene(object):
     
     def enter(self):
-        print("Scene has not been set up yet.")
+        print("This scene is not yet configured.")
+        print("Subclass it and implement enter().")
         exit(1)
-    
+        
 class Engine(object):
-
+    
     def __init__(self, scene_map):
         self.scene_map = scene_map
         
@@ -23,188 +24,227 @@ class Engine(object):
             
         # be sure to print out the last scene
         current_scene.enter()
-    
+        
 class Death(Scene):
     
     quips = [
-        "You got shot in the face.",
-        "You really suck at this game.",
-        "You are actually hot garbage at this game.",
-        "Holy cow how bad can you be at this game?",
-        "My grandma can do better than you."
+        "You died. You kinda suck at this.",
+        "Your Mom would be proud...if she were smarter.",
+        "Such a luser.",
+        "I have a small puppy that's better at this.",
+        "You're worse than your Dad's jokes."
         
         ]
-    
+        
     def enter(self):
-        # pass
         print(Death.quips[randint(0, len(self.quips)-1)])
         exit(1)
-class SpaceShip(Scene):
+            
+class CentralCorridor(Scene):
     
     def enter(self):
-        # pass
         print(dedent("""
-            The Space Pirates have invaded your planet and they destroyed your whole planet.
-            You are the only survivor of your kind on your planet. Your last mission is to destroy
-            the Space Pirates and they took your planet secret to destroying the whole universe and
-            you have to get it back at all cost. You saw their ship about to take off, so you dash
-            towards their ship and Brukz, a soilder of The Space Pirates stopped you. Brukz is a tall
-            red octopus, black gooey teeth, and a eyepatch that has a laser attach to it. 
+            The Gothons of Planet Percal #25 have invaded your ship and
+            destroyed your entire crew. You are the last surviving
+            member and your last mission is to get the neutron destruct
+            bomb from the Weapons Armory, put it in the bridge, and
+            blow the ship up after getting into an escape pod.
+
+            You're running down the central corridor to the Weapons
+            Armory when a Gothon jumps out, red scaly skin, dark grimy
+            teeth, and evil clown costume flowing around his hate
+            filled body. He's blocking the door to the Armory and
+            about to pull a weapon to blast you.
             """))
-        
+            
         action = input("> ")
         
-        if action == "Fire":
+        if action == "shoot!":
             print(dedent("""
-                You quickly pull out your blaster and fire at Brukz. He dodges it and shoots you
-                in the ribs and you got injured then he shoots you multiple times and you die and
-                he took your body and gave it to his to his friend and his friend ate you.
+                Quick on the draw you yank out your blaster and fire
+                it at the Gothon. His clown costume is flowing and
+                moving around his body, which throws off your aim.
+                Your laser hits his costume but misses him entirely.
+                This completely ruins his brand new costume his mother
+                bought him, which makes him fly into an insane rage
+                and blast you repeatedly in the face until you are
+                dead. Then he eats you.
+                """))
+            return 'death'
+        
+        elif action == "dodge!":
+            print(dedent("""
+                Like a world class boxer you dodge, weave, slip and
+                slide right as the Gothon's blaster cranks a laser
+                past your head. In the middle of your artful dodge
+                your foot slips and you bang your head on the metal
+                wall and pass out. You wake up shortly after only to
+                die as the Gothon stomps on your head and eats you.
                 """))
             return 'death'
             
-        elif action == "Block":
+        elif action == "tell a joke":
             print(dedent("""
-                You take out your magic barrier and block Brukz shot and you shoot him in his eye.
-                Brukz started screaming in pain and you shoot him again in his other eye and he dropped
-                to the ground and blood started coming out both of his eyes and he died to blood lost.
-                """)) 
-            return 'SpaceshipCode'
-        
-        else:
-            print("Ыоу аре тчоу ступид.")
-            return 'SpaceShip'
+                Lucky for you they made you learn Gothon insults in
+                the academy. You tell the one Gothon joke you know:
+                Lbhe zbgure vf fb sng, jura fur fvgf nebhaq gur ubhfr,
+                fur fvgf nebhaq gur ubhfr. The Gothon stops, tries
+                not to laugh, then busts out laughing and can't move.
+                While he's laughing you run up and shoot him square in
+                the head putting him down, then jump through the
+                Weapon Armory door.
+                """))
+            return 'laser_weapon_armory'
             
-class SpaceshipCode(Scene):
+        else:
+            print("DOES NOT COMPUTE!")
+            return 'central_corridor'
+                 
+class LaserWeaponArmory(Scene):
     
     def enter(self):
-        # pass
         print(dedent("""
-            You starting running to the ship and you dolphin dive into the ship last second before the door. 
-            You crouch behind and box and you scan the room for more evil doers.
-            You see all of the evil doers leave the room that your in to a whole different room
-            and you see one placing down your planet secret to destroy the universe and once he leave
-            you run silently to the box. You find out the box has a 3 digit code and if you get it wrong 4 times
-            then the box makes a loud noise.
-            """)) 
-        
-        # code = f"{randint(1,4)}{randint(1,4)}{randint(1,4)}"
-        code = "123"
+            You do a dive roll into the Weapon Armory, crouch and scan
+            the room for more Gothons that might be hiding. It's dead
+            quiet, too quiet. You stand up and run to the far side of
+            the room and find the neutron bomb in its container.
+            There's a keypad lock on the box and you need the code to
+            get the bomb out. If you get the code wrong 10 times then
+            the lock closes forever and you can't get the bomb. The
+            code is 3 digits.
+            """))
+            
+        code = f"{randint(1,9)}{randint(1,9)}{randint(1,9)}"
         guess = input("[keypad]> ")
         guesses = 0
         
-        while guess != code and guesses < 25:
-            print("BZZZZEDDD!")
+        while guess != code and guesses <10:
+            print("BZZZZZEEEDDD!")
             guesses += 1
             guess = input("[keypad]> ")
             
-        if guess == code:
-            print(dedent("""
-                A green text appears and you get access to the box with your planet secret. 
-                A white fog appears out of the box and you grab your planet secret and you
-                blast the door open and you jump before the spaceship started deploying. To
-                destroy the universe you have to activate it at the Cathedral. 
-                """))
-            return 'Cathedral'
+            if guess == code:
+               print(dedent("""
+                    The container clicks open and the seal breaks, letting
+                    gas out. You grab the neutron bomb and run as fast as
+                    you can to the bridge where you must place it in the
+                    right spot.
+                    """))
+            return 'the_bridge'
         else:
-            print(dedent("""
-                The box makes a loud noise alerting all of the evil doers and they all rush to you. 
-                You can't kill them all, so you got shot multiple times by the evil doers while taking 
-                only 4 down and you die.
-                """))
-            return 'death'
-            
-            
-class  Cathedral(Scene):
+                print(dedent("""
+                    The lock buzzes one last time and then you hear a
+                    sickening melting sound as the mechanism is fused
+                    together. You decide to sit there, and finally the
+                    Gothons blow up the ship from their ship and you die.
+                    """))
+                return 'death'
+                
+class TheBridge(Scene):
     
     def enter(self):
-        # pass
         print(dedent("""
-            You bust open the Cathedral door with your blaster and you place the
-            planet secret in the middle of the circle. You hear footsteps outside
-            and a bunch of evil doers start shooting at you but you dodge all of the shots
-            and the last shot almost hit you, but you quickly use your magic barrier. They keep
-            shooting at you and you keep dodging and you started getting tired so you hide behind a broken
-            stone wall
+            You burst onto the Bridge with the netron destruct bomb
+            under your arm and surprise 5 Gothons who are trying to
+            take control of the ship. Each of them has an even uglier
+            clown costume than the last. They haven't pulled their
+            weapons out yet, as they see the active bomb under your
+            arm and don't want to set it off.
             """))
             
         action = input("> ")
         
-        if action == "Keep Dodging":
-           print(dedent("""
-                You slowly start to get tired from dodging all of the lasers and you get shot in the legs, arms, ribs,
-                and the head. You bleed out and die and they throw you into space back on their spaceships. 
-                """))
-           return 'death'
-            
-        elif action == "Grerrier":
+        if action == "throw the bomb":
             print(dedent("""
-                You use your magic barrier to block all the shots and you throw a magic grenade. The enemy doesn't
-                see the magic grenade because they are focus on killing you. The enemy gets blown up and you are safe
-                to destroy the whole universe. 
+                In a panic you throw the bomb at the group of Gothons
+                and make a leap for the door. Right as you drop it a
+                Gothon shoots you right in the back killing you. As
+                you die you see another Gothon frantically try to
+                disarm the bomb. You die knowing they will probably
+                blow up when it goes off.
                 """))
-                
-            return 'UniverseDestroyed'
+            return 'death'
+            
+        elif action == "slowly place the bomb":
+            print(dedent("""
+            You point your blaster at the bomb under your arm and
+            the Gothons put their hands up and start to sweat.
+            You inch backward to the door, open it, and then
+            carefully place the bomb on the floor, pointing your
+            blaster at it. You then jump back through the door,
+            punch the close button and blast the lock so the
+            Gothons can't get out. Now that the bomb is placed
+            you run to the escape pod to get off this tin can.
+            """))
         
-    
-class UniverseDestroyed(Scene):
+            return 'escape_pod'
+        else:
+            print("DOES NOT COMPUTE!")
+            return"the_bridge" 
+            
+class EscapePod(Scene):
     
     def enter(self):
-        # pass
-        print(dedent(f"""
-            You start your trial on destroying the whole universe but you start to see a list and the list
-            says,
-            1.) Бецоме Год
-            2.) Дие 
-            you can't read the list because its in a language you don't know. You have to
-            put a number in from the list and see what happens.
+        print(dedent("""
+            You rush through the ship desperately trying to make it to
+            the escape pod before the whole ship explodes. It seems
+            like hardly any Gothons are on the ship, so your run is
+            clear of interference. You get to the chamber with the
+            escape pods, and now need to pick one to take. Some of
+            them could be damaged but you don't have time to look.
+            There's 5 pods, which one do you take?
             """))
-        weird_list = randint(1,2)
-        guess = input("[list #]> ")
+            
+        good_pod = randint(1,5)
+        guess = input("[pod #]> ")
         
-        if int(guess) != weird_list:
-            print(dedent(f"""
-                You choose {guess} and you become god and you can create your own
-                world with your own people and basically do anything you want. You
-                are the ultimate being.
+        if int(guess) != good_pod:
+            print(dedent("""
+                You jump into pod {guess} and hit the eject button.
+                The pod escapes out into the void of space, then
+                implodes as the hull ruptures, crushing your body into
+                jam jelly.
                 """))
-            return 'Finished'
+            return 'death'
         else:
-            print(dedent(f"""
-                You choose {guess} and you just suddenly start crumbling and you feel pain
-                and you just vanish. 
+            print(dedent("""
+                You jump into pod {guess} and hit the eject button.
+                The pod easily slides out into space heading to the
+                planet below. As it flies to the planet, you look
+                back and see your ship implode then explode like a
+                bright star, taking out the Gothon ship at the same
+                time. You won!
                 """))
                 
-            return 'death'
-    
+            return 'finished'
+            
 class Finished(Scene):
     
     def enter(self):
-        print("Congrats Legend, You Won!")
-        return 'Finished'
+        print("You won! Good job.")
+        return 'finished'
+        
 class Map(object):
     
     scenes = {
-        'SpaceShip': SpaceShip(),
-        'SpaceshipCode': SpaceshipCode(),
-        'Cathedral': Cathedral(),
-        'UniverseDestroyed': UniverseDestroyed(),
-        'Finished': Finished(),
-        'death': Death(), 
-        
+        'central_corridor': CentralCorridor(),
+        'laser_weapon_armory': LaserWeaponArmory(),
+        'the_bridge': TheBridge(),
+        'escape_pod': EscapePod(),
+        'death': Death(),
+        'finished': Finished(),
     }
     
     def __init__(self, start_scene):
-        # pass
         self.start_scene = start_scene
-    
+        
     def next_scene(self, scene_name):
-        # pass
         val = Map.scenes.get(scene_name)
         return val
-    def opening_scene(self):
-        # pass
-        return self.next_scene(self.start_scene)
     
-a_map = Map('SpaceShip')
+    def opening_scene(self):
+        return self.next_scene(self.start_scene)
+        
+a_map = Map('central_corridor')
 a_game = Engine(a_map)
 a_game.play()
